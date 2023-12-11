8eade9c11025a86f45676ee3f9e5798229546fc7
Chen Guo <c82guo@uwaterloo.ca>
2023-12-10 22:57:19 -0500

    test

---
+++ terraform/eip.tf
@@ -0,0 +1,8 @@
+# 其实这一部很简单：就是确认要有一个eip依赖于Internet gateway即可
+resource "aws_eip" "eip_1" {
+  depends_on = [ aws_internet_gateway.igw ]
+}
+
+resource "aws_eip" "eip_2" {
+  depends_on = [ aws_internet_gateway.igw ]
+}
