7d6658ffc94bb092950ca5d7085e898f7a885de2
Chen Guo <c82guo@uwaterloo.ca>
2024-02-07 16:11:42 -0500

    add main.yml file to start

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+// 首先要新建一个文件夹terraform
+// This file is the entry point
+terraform {
+  required_providers { // 应用平台
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.0"
+    }
+  }
+
+  // backend连接的resourses
+  backend "s3" {
+    
+  }
+}
+
+provider "aws" {
+  region = "${var.aws.region}"
+}
